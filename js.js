1ecdfe4dfba891aa4747d4a83e56985857a17e69
Devin Daniels <devingbusiness@gmail.com	git config --global init.defaultBranch mainclear>
2022-07-26 13:06:31 -0700

    Refactored JS file

--- js.js
+++ js.js
@@ -5,67 +5,55 @@ const DEFAULT_SIZE = 32;
 let currentColor = DEFAULT_COLOR
 let currentMode = DEFAULT_MODE
 let currentSize = DEFAULT_SIZE
+var sketchContainer = null; // stores address for the current etch-a-sketch
 
-
-
-const gridParent = document.querySelector("#parent-for-grid");
-const reset = document.getElementById("reset-pad");
-const colorPicker = document.getElementById('color-picker')
-const eraser = document.getElementById('eraser')
-const rangeSlider = document.getElementById("range-slider");
-const sketchDimensions = document.getElementById("display-grid-dimensions");
-
-
+// tracks whether mouse is in 'clicked' position
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
+// gridParent is the container for the etch-a-sketch
+const gridParent = document.querySelector("#parent-for-grid");
+// variable that holds the reset etch-a-sketch button
+const reset = document.getElementById("reset-pad");
+// variable that stores the color value 
+const colorPicker = document.getElementById('color-picker');
+// stores the current mode of the etch-a-sketch to be eraser 
+const eraser = document.getElementById('eraser');
+// variable for the range slider UI button 
+const rangeSlider = document.getElementById("range-slider");
+// variable for updating the current dimensions of the etch-a-sketch
+const sketchDimensions = document.getElementById("display-grid-dimensions");
 
 
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
 eraser.onclick = (e) => setCurrentColor("#FFFFFF")
 reset.onclick = () => reloadGrid();
-
-// this gets the value from the rangeSlider 
-
-rangeSlider.oninput = function() {
-    currentSize = rangeSlider.value;
-    console.log(currentSize);
-    sketchDimensions.textContent = currentSize + " x " + currentSize;
-    createSketchPad();
-}
-
-var gridContainer;
-window.onload = function() {
-    gridContainer = document.createElement('div');
-    sketchDimensions.textContent = currentSize + " x " + currentSize;
-    createGridContainer();
-    addDivs();
-};
-
-
-function createGridContainer() {
-    var newContainer = document.createElement('div');
-    newContainer.style.width = "500px";
-    newContainer.style.height = "500px";
-    newContainer.style.margin = "0";
-    newContainer.style.padding = "0"
-    newContainer.style.display = "grid";
-    newContainer.style.border = "2px solid black";
-    newContainer.style.setProperty('grid-template-columns', 'repeat(' + currentSize + ', 1fr)');
-    gridContainer = newContainer;
-}
+rangeSlider.oninput = () => updateDimensions();
 
 
 function createSketchPad() {
-    // remove the old div container
-    gridParent.removeChild(gridContainer);
+
+    if (sketchContainer !== null) {
+        // remove the old div container
+        gridParent.removeChild(sketchContainer);
+    }
     // // create a new grid div container with correct dimension
-    createGridContainer();
-    addDivs();
+    var newSketchPad = document.createElement('div');
+    newSketchPad.style.display = "grid";
+    newSketchPad.style.width = "500px";
+    newSketchPad.style.height = "500px";
+    newSketchPad.style.margin = "0";
+    newSketchPad.style.padding = "0"
+    newSketchPad.style.border = "2px solid black";
+    newSketchPad.style.setProperty('grid-template-columns', 'repeat(' + currentSize + ', 1fr) ');
+    // update the sketchContainer to be the newSketchPad
+    sketchContainer = newSketchPad
+        // populate the new sketch container with pixels
+    populateSketchPad();
 }
 
-function addDivs() {
+function populateSketchPad() {
     for (var i = 0; i < currentSize * currentSize; i++) {
         var newDiv = document.createElement('div');
         newDiv.className = "template-div";
@@ -75,23 +63,37 @@ function addDivs() {
         newDiv.style.margin = 0;
         newDiv.style.padding = 0;
         newDiv.addEventListener('mouseover', changeColor)
-        gridContainer.appendChild(newDiv);
+        sketchContainer.appendChild(newDiv);
     }
-    gridParent.append(gridContainer);
+    // Sketch container is completed, append it to its parent container
+    gridParent.append(sketchContainer);
+}
+
+function updateDimensions() {
+    // save the new size
+    currentSize = rangeSlider.value;
+    // display the new dimensions to the user
+    sketchDimensions.textContent = currentSize + " x " + currentSize;
+    // create the new sketch pad 
+    createSketchPad();
 }
 
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
     e.target.style.backgroundColor = currentColor;
-
 }
 
 function reloadGrid() {
     createSketchPad();
 }
 
-
-
 function setCurrentColor(color) {
     currentColor = color;
 }
+
+window.onload = () => {
+    // display the current dimensions
+    sketchDimensions.textContent = currentSize + " x " + currentSize;
+    // create a new sketch pad 
+    createSketchPad()
+};
