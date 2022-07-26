45b89e5fd9204281b721f2d42e6aa7ff5fbeb7c3
Devin Daniels <devingbusiness@gmail.com	git config --global init.defaultBranch mainclear>
2022-07-26 12:21:06 -0700

    Added more click and drag functionality.

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 const DEFAULT_COLOR = '#000000'
 const DEFAULT_MODE = 'color'
-const DEFAULT_SIZE = 16
+const DEFAULT_SIZE = 32;
 
 let currentColor = DEFAULT_COLOR
 let currentMode = DEFAULT_MODE
@@ -16,7 +16,6 @@ const eraser = document.getElementById('eraser')
 
 
 
-
 function setCurrentColor(color) {
     currentColor = color;
 }
@@ -25,8 +24,13 @@ colorPicker.oninput = (e) => setCurrentColor(e.target.value);
 eraser.onclick = (e) => setCurrentColor("#FFFFFF")
 
 
-var gridContainer;
-var colSize = 32;
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+
+
 
 clearSketch.onclick = () => reloadGrid();
 
@@ -36,16 +40,16 @@ var slider = document.getElementById("myRange");
 var displayCurrentSize = document.querySelector(".display-size");
 
 slider.oninput = function() {
-    colSize = slider.value;
-    console.log(colSize);
-    displayCurrentSize.textContent = colSize + " x " + colSize;
+    currentSize = slider.value;
+    console.log(currentSize);
+    displayCurrentSize.textContent = currentSize + " x " + currentSize;
     createSketchPad();
 }
 
-
+var gridContainer;
 window.onload = function() {
     gridContainer = document.createElement('div');
-    displayCurrentSize.textContent = colSize + " x " + colSize;
+    displayCurrentSize.textContent = currentSize + " x " + currentSize;
     createGridContainer();
     addDivs();
 };
@@ -59,7 +63,7 @@ function createGridContainer() {
     newContainer.style.padding = "0"
     newContainer.style.display = "grid";
     newContainer.style.border = "2px solid black";
-    newContainer.style.setProperty('grid-template-columns', 'repeat(' + colSize + ', 1fr)');
+    newContainer.style.setProperty('grid-template-columns', 'repeat(' + currentSize + ', 1fr)');
     gridContainer = newContainer;
 }
 
@@ -73,7 +77,7 @@ function createSketchPad() {
 }
 
 function addDivs() {
-    for (var i = 0; i < colSize * colSize; i++) {
+    for (var i = 0; i < currentSize * currentSize; i++) {
         var newDiv = document.createElement('div');
         newDiv.className = "template-div";
         newDiv.style.height = "auto";
@@ -82,18 +86,15 @@ function addDivs() {
         newDiv.style.margin = 0;
         newDiv.style.padding = 0;
         newDiv.addEventListener('mouseover', changeColor)
-        newDiv.addEventListener('mousedown', changeColor)
         gridContainer.appendChild(newDiv);
     }
     gridParent.append(gridContainer);
 }
 
-
-
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
-
     e.target.style.backgroundColor = currentColor;
+
 }
 
 function reloadGrid() {
