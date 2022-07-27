4215d7c7b21429887caf70c91744637281efd3b5
Devin Daniels <devingbusiness@gmail.com	git config --global init.defaultBranch mainclear>
2022-07-27 11:33:56 -0700

    Project completed ✅

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 const DEFAULT_COLOR = '#000000'
 const DEFAULT_MODE = 'color'
 const DEFAULT_SIZE = 32;
+const DEFAULT_BUTTON_COLOR = "rgb(10,89,250)";
 
 let currentColor = DEFAULT_COLOR
 let currentMode = DEFAULT_MODE
@@ -14,6 +15,8 @@ const reset = document.getElementById("reset-pad");
 // variable that stores the color value 
 const colorPicker = document.getElementById('color-picker');
 // variable that stores the color button toggle
+const rainbowButtonToggle = document.getElementById('rainbow-toggle');
+
 const colorButtonToggle = document.getElementById('color-toggle')
     // stores the current mode of the etch-a-sketch to be eraser 
 const eraserButtonToggle = document.getElementById('eraser');
@@ -27,8 +30,15 @@ colorPicker.oninput = (e) => {
     updateCurrentMode('color');
     currentColor = e.target.value;
 };
-colorButtonToggle.onclick = () => updateCurrentMode('color');
-eraserButtonToggle.onclick = () => updateCurrentMode('eraser');
+rainbowButtonToggle.onclick = () => {
+    updateCurrentMode('rainbow');
+}
+colorButtonToggle.onclick = () => {
+    updateCurrentMode('color');
+}
+eraserButtonToggle.onclick = () => {
+    updateCurrentMode('eraser');
+}
 reset.onclick = () => reloadGrid();
 rangeSlider.oninput = () => updateDimensions();
 
@@ -60,8 +70,8 @@ function updateDimensions() {
     currentSize = rangeSlider.value;
     // display the new dimensions to the user
     sketchDimensions.textContent = currentSize + " x " + currentSize;
-    // reset current mode to color
-    updateCurrentMode('color');
+    // update the current mode 
+    updateCurrentMode(currentMode);
     // create the new sketch pad 
     createSketchPad();
 }
@@ -70,7 +80,12 @@ function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) {
         return;
     }
-    if (currentMode === 'color') {
+    if (currentMode === 'rainbow') {
+        const r = Math.floor(Math.random() * 256)
+        const g = Math.floor(Math.random() * 256)
+        const b = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    } else if (currentMode === 'color') {
         e.target.style.backgroundColor = currentColor;
     } else {
         e.target.style.backgroundColor = '#fefefe';
@@ -78,30 +93,53 @@ function changeColor(e) {
 }
 
 function updateCurrentMode(updatedMode) {
-    // toggle off current mode
+
+    // update the highlighted color choice 
+    if (updatedMode === 'color') {
+        colorButtonToggle.style.backgroundColor = DEFAULT_BUTTON_COLOR;
+        eraserButtonToggle.style.backgroundColor = "white";
+        rainbowButtonToggle.style.backgroundColor = "white";
+    } else if (updatedMode === 'rainbow') {
+        rainbowButtonToggle.style.backgroundColor = DEFAULT_BUTTON_COLOR;
+        colorButtonToggle.style.backgroundColor = "white";
+        eraserButtonToggle.style.backgroundColor = "white";
+    } else if (updatedMode === 'eraser') {
+        eraserButtonToggle.style.backgroundColor = DEFAULT_BUTTON_COLOR;
+        rainbowButtonToggle.style.backgroundColor = "white";
+        colorButtonToggle.style.backgroundColor = "white";
+    }
+
+    // toggle off current mode active class 
     if (currentMode === 'color') {
         colorButtonToggle.classList.remove = 'active';
     } else if (currentColor === 'eraser') {
         eraserButtonToggle.classList.remove = 'active';
+    } else if (currentMode === 'rainbow') {
+        rainbowButtonToggle.classList.remove = 'active';
     }
     // update current mode and make active
     if (updatedMode === 'color') {
         colorButtonToggle.classList.add = 'active';
     } else if (updatedMode === 'eraser') {
         eraserButtonToggle.classList.add = 'active';
+    } else if (updatedMode === 'rainbow') {
+        rainbowButtonToggle.classList.add = 'active';
     }
+    // assign updated mode 
     currentMode = updatedMode;
 }
 
-
 function reloadGrid() {
+    // update the current mode 
+    updateCurrentMode(currentMode);
+    // reset the grid content
     gridParent.innerHTML = '';
-    updateCurrentMode('color');
+    // create a new sketch
     createSketchPad();
 }
 
-
 window.onload = () => {
+    colorButtonToggle.style.backgroundColor = DEFAULT_BUTTON_COLOR;
     // display the current dimensions
     sketchDimensions.textContent = currentSize + " x " + currentSize;
     // create a new sketch pad 
